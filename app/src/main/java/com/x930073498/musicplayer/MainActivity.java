173a59f0f8e16e20f94a6175560bd63fe402f87d
x930073498 <x930073498@gmail.com>
2017-04-26 00:31:31 +0800

    test

--- app/src/main/java/com/x930073498/musicplayer/MainActivity.java
+++ app/src/main/java/com/x930073498/musicplayer/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -17,9 +17,9 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-//        TextView tv = (TextView) findViewById(R.id.sample_text);
-//        tv.setText(stringFromJNI());
+//         Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
     }
 
     /**
---
+++ javalib/src/main/java/com/x930073498/MyClass.java
@@ -0,0 +1,4 @@
+package com.x930073498;
+
+public class MyClass {
+}

e4d3df0e792879094341c49b9fca2b239c63470b
x930073498 <x930073498@gmail.com>
2017-04-26 00:16:02 +0800

    test

--- app/src/main/java/com/x930073498/musicplayer/MainActivity.java
+++ app/src/main/java/com/x930073498/musicplayer/MainActivity.java
@@ -1,10 +1,11 @@
 package com.x930073498.musicplayer;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     // Used to load the 'native-lib' library on application startup.
     static {

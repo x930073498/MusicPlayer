14917561b32588f34820bdf4edc2c75009d7641f
x930073498 <zq1092742573>
2017-04-26 09:26:55 +0800

    test

--- app/src/main/java/com/x930073498/musicplayer/MainActivity.java
+++ app/src/main/java/com/x930073498/musicplayer/MainActivity.java
@@ -21,8 +21,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //         Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+//        TextView tv = (TextView) findViewById(R.id.sample_text);
+//        tv.setText(stringFromJNI());
     }
 
     /**

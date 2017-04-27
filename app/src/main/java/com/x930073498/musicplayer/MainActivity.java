43a239baa2fcf93e28bd002ad76cd7321dbe2e75
x930073498 <zq1092742573>
2017-04-27 18:26:20 +0800

    test

--- app/src/main/java/com/x930073498/musicplayer/MainActivity.java
+++ app/src/main/java/com/x930073498/musicplayer/MainActivity.java
@@ -1,25 +1,61 @@
 package com.x930073498.musicplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+
+import com.bumptech.glide.Glide;
+import com.caimuhao.rxpicker.RxPicker;
+import com.caimuhao.rxpicker.bean.ImageItem;
+import com.hwangjr.rxbus.RxBus;
+import com.jakewharton.rxbinding2.support.v7.widget.RxRecyclerView;
+import com.jakewharton.rxbinding2.support.v7.widget.RxRecyclerViewAdapter;
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
+import com.x930073498.musicplayer.databinding.ActivityMainBinding;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.subjects.AsyncSubject;
+import io.reactivex.subjects.PublishSubject;
+import sj.mblog.L;
 
 
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends RxAppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        RxBus.get().register(this);
         setContentView(R.layout.activity_main);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setText("测试");
+
+        RxPicker.init((imageView, path, width, height) -> Glide.with(imageView.
+                getContext())
+                .load(path)
+                .override(width, height)
+                .dontAnimate()
+                .centerCrop()
+                .into(imageView));
+
 //         Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+//        TextView tv = (TextView) findViewById(R.id.sample_text);
+//        tv.setOnClickListener((v) -> {
+//
+//        });
+
+//        tv.setText(stringFromJNI());
     }
 
     /**
@@ -27,4 +63,19 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        RxBus.get().unregister(this);
+    }
+
+
+//    public void select(View view) {
+//        RxPicker.of().single(false).camera(true).limit(3, 6).start(this).subscribe(imageItems -> Flowable.fromIterable(imageItems).map(ImageItem::getPath).subscribe(L::d));
+//    }
+
+    public void onClick(View view) {
+        RxPicker.of().single(false).camera(false).limit(0, 3).start(this).subscribe(imageItems -> L.d(Flowable.fromIterable(imageItems).blockingFirst().getPath()), Throwable::printStackTrace);
+    }
 }
---
+++ javalib/src/main/java/Test.java
@@ -0,0 +1,55 @@
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
+
+/**
+ * Created by x930073498 on 17-4-27.
+ */
+
+public class Test {
+    public static void main(String... arg0) {
+//        Observable.create((ObservableOnSubscribe<Integer>) observer -> {
+//            try {
+//                if (!observer.isDisposed()) {
+//                    for (int i = 0; i < 5; i++) {
+//                        observer.onNext(i);
+//                    }
+//                    observer.onComplete();
+//
+//                }
+//            } catch (Exception e) {
+//                observer.onError(e);
+//            }
+//        }).subscribe(System.out::println, e -> System.out.println("this is onError"), () -> System.out.println("onComplete"));
+//
+//        Observable.defer(() -> new Observable<Integer>() {
+//
+//            @Override
+//            protected void subscribeActual(Observer<? super Integer> observer) {
+//                try {
+//                    for (int i = 0; i < 5; i++) {
+//                        observer.onNext(i);
+//                    }
+//                    observer.onComplete();
+//                } catch (Exception e) {
+//                    observer.onError(e);
+//                }
+//            }
+//        }).subscribe(System.out::println, e -> System.out.println("this is onError"), () -> System.out.println("onComplete"));
+
+        Integer[] items = {0, 1, 2, 3, 4, 5};
+        Observable.fromArray(items).subscribe(System.out::println, Throwable::printStackTrace, () -> System.out.print("onComplete"));
+        Observable.timer(2, TimeUnit.SECONDS).subscribe(System.out::println);
+//        Observable.just(items,items,items).blockingIterable().forEach((integers) -> Observable.fromArray(integers).subscribe(System.out::println));
+        Observable.just(items, items, items).blockingSubscribe(integers -> Flowable.fromArray(integers).subscribe(System.out::println));
+    }
+}
